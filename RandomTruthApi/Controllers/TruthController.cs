563600110d59b37daa9cca582e3e32733b391505
YuriiProniuk <georgeproniuk@gmail.com>
2022-09-14 18:02:29 +0300

    Remove redundant code

--- RandomTruthApi/Controllers/TruthController.cs
+++ RandomTruthApi/Controllers/TruthController.cs
@@ -12,9 +12,6 @@ namespace RandomTruthApi.Controllers
     {
         private readonly ILogger<TruthController> _logger;
 
-        // The Web API will only accept tokens 1) for users, and 2) having the "access_as_user" scope for this API
-        static readonly string[] scopeRequiredByApi = new string[] { "access_as_user" };
-
         public TruthController(ILogger<TruthController> logger)
         {
             _logger = logger;
